94f48bb359bdb884ee6b3fa6c90362c965c27a91
Michael Adkins <Michael.adkins2@gmail.com>
2016-04-22 06:25:23 -0400

    Added files via upload

--- javascript.js
+++ javascript.js
@@ -1,20 +1,21 @@
 numofPorts=false;
 numofReadings=3;
+numofColumns=6;
+numofRows=3;
 function sixPort() {
 	sixPortcall=document.getElementById("sixPort").checked;
-		if (sixPortcall===true){
+		if (sixPortcall==true){
 			document.getElementById("sixPort1").style.display="";
 			document.getElementById("cell6.0").style.display="";
 			document.getElementById("cell6.1").style.display="";
 			document.getElementById("cell6.2").style.display="";
 			document.getElementById("cell6.3").style.display="";
 			document.getElementById("cell6.4").style.display="";
-			document.getElementById("total6").style.display="";
 			numofPorts=true;
 		}
 }
 function getDims() {
-	var width, heigth, thickness, area, port;
+	var width, heigth, thickness;
 	width= document.getElementById("width");
 	heigth=document.getElementById("heigth");
 	thickness=document.getElementById("topPlate");
@@ -22,7 +23,7 @@ function getDims() {
 	heigth=parseFloat(heigth.value);
 	thickness=parseFloat(thickness.value);
 	area=(width*heigth) / 144;
-	area=area.toFixed(2);
+	area=parseFloat(area.toFixed(2));
 	document.getElementById("area").innerHTML="Area: " + area +" sq.ft.";
 	secondMark=heigth / 2 + thickness;
 	firstMark=heigth/4 + thickness;
@@ -31,28 +32,32 @@ function getDims() {
 	document.getElementById("secondMark").innerHTML="Second mark: " + secondMark.toFixed(2)+"\"";
 	document.getElementById("thirdMark").innerHTML="Third mark: " + thirdMark.toFixed(2)+"\"";
 	perPort= document.getElementById("perPort").checked;
-		if (perPort===true) {
+		if (perPort==true) {
 			document.getElementById("fiveMeas").style.display="";
 			document.getElementById("fiveMeas2").style.display="";
 			numofReadings=5;
 		}
 	sixPort();	
+	if (numofPorts==true) {numofColumns=7;}
+
+	if (numofReadings===5) {numofRows=5;}
 }
 measArray= [];
-numofColumns=6;
-numofRows=3;
-if (numofPorts===true) {numofColumns=8}
 
-if (numofReadings==5) {numofRows=5}
+
 
 function totals(){
-var totals1,totals2,totals3,totals4,totals5,totals6;
+var totals1;
 totals1=0;
-for (k=0; k<measArray.length/*numofReadings*/;k++){
+for (k=0; k<measArray.length;k++){
 	totals1+=measArray[k];
 }
 totals1=parseFloat(totals1);
 document.getElementById("total1").innerHTML=totals1;
+average=parseFloat(Math.round(totals1/measArray.length));
+document.getElementById("average").innerHTML=average;
+cfm=parseFloat(Math.round(average*area));
+document.getElementById("cfm").innerHTML=cfm;
 }
 
 	
@@ -63,9 +68,7 @@ function measurements(){
 			meas=  prompt("enter meaurement");
 			meas = parseFloat(Math.round((Math.sqrt(meas))*4005));
 			measArray.push(meas);
-			/*y=(measArray.length)-1;
-			x=measArray[y];*/
-			document.getElementById("cell"+j+"."+i).innerHTML=meas;//Math.round((Math.sqrt(x))*4005);
+			document.getElementById("cell"+j+"."+i).innerHTML=meas;
 		}
 	}
 	totals();
